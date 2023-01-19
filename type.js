6ee18bb524a25d6f42ce05c494c6c2c841ab3814
balaji-ravi2104 <108446931+balaji-ravi2104@users.noreply.github.com>
2023-01-19 10:04:13 +0530

    Add files via upload

---
+++ type.js
@@ -0,0 +1,7 @@
+
+<script type="text/javascript">
+    window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+    }) 
+</script> 
